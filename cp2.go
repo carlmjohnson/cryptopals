d2a884c3e03de1e39d8d8dedd6752da221143752
Carl Johnson <me@carlmjohnson.net>
2019-03-15 10:58:59 -0400

    challenge 10

--- cp2.go
+++ cp2.go
@@ -1,5 +1,7 @@
 package cryptopals
 
+import "crypto/aes"
+
 func PKCSPadding(b []byte, padding int) []byte {
 	remainder := len(b) % padding
 	if remainder == 0 {
@@ -12,4 +14,34 @@ func PKCSPadding(b []byte, padding int) []byte {
 		r[len(r)-1-i] = '\x04'
 	}
 	return r
+}
+
+func CBCEncrypt(plaintext, key, iv []byte) []byte {
+	block, err := aes.NewCipher(key)
+	die(err)
+
+	size := block.BlockSize()
+	src := PKCSPadding(plaintext, size)
+	dst := make([]byte, len(src))
+	last := iv
+	for i := 0; i < len(dst); i += size {
+		block.Encrypt(dst[i:], XorFixed(src[i:i+size], last))
+		last = dst[i : i+size]
+	}
+	return dst
+}
+
+func CBCDecrypt(cipher, key, iv []byte) []byte {
+	block, err := aes.NewCipher(key)
+	die(err)
+
+	size := block.BlockSize()
+	dst := make([]byte, len(cipher))
+	last := iv
+	for i := 0; i < len(dst); i += size {
+		block.Decrypt(dst[i:], cipher[i:])
+		copy(dst[i:], XorFixed(dst[i:i+size], last))
+		last = cipher[i : i+size]
+	}
+	return dst
 }
--- cp2_test.go
+++ cp2_test.go
@@ -1,6 +1,9 @@
 package cryptopals
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Test9(t *testing.T) {
 	tcs := []struct {
@@ -39,6 +42,61 @@ func Test9(t *testing.T) {
 			have := PKCSPadding([]byte(tc.input), tc.padding)
 			expect := []byte(tc.output)
 			equalBytes(t, have, expect)
+		})
+	}
+}
+
+func Test10a(t *testing.T) {
+	tcs := []struct {
+		name  string
+		input string
+		key   string
+		iv    string
+	}{
+		{
+			name:  "roundtrip",
+			input: "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.",
+			key:   "YELLOW SUBMARINE",
+			iv:    strings.Repeat("\x00", 16),
+		},
+		{
+			name:  "one block",
+			input: "................",
+			key:   "YELLOW SUBMARINE",
+			iv:    "YELLOW SUBMARINE",
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			cipher := CBCEncrypt([]byte(tc.input), []byte(tc.key), []byte(tc.iv))
+			have := CBCDecrypt(cipher, []byte(tc.key), []byte(tc.iv))
+			expect := PKCSPadding([]byte(tc.input), len(tc.key))
+			equalString(t, string(have), string(expect))
+		})
+	}
+}
+
+func Test10b(t *testing.T) {
+	tcs := []struct {
+		name   string
+		file   string
+		key    string
+		iv     string
+		expect string
+	}{
+		{
+			name:   "roundtrip",
+			file:   "10.txt",
+			key:    "YELLOW SUBMARINE",
+			iv:     strings.Repeat("\x00", 16),
+			expect: "I'm back and I'm ringin' the bell \nA rockin' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that's my DJ Deshay cuttin' all them Z's \nHittin' hard and the girlies goin' crazy \nVanilla's on the mike, man I'm not lazy. \n\nI'm lettin' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse's to the side yellin', Go Vanilla Go! \n\nSmooth 'cause that's the way I will be \nAnd if you don't give a damn, then \nWhy you starin' at me \nSo get off 'cause I control the stage \nThere's no dissin' allowed \nI'm in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n' play \n\nStage 2 -- Yea the one ya' wanna listen to \nIt's off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI'm an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI'm like Samson -- Samson to Delilah \nThere's no denyin', You can try to hang \nBut you'll keep tryin' to get my style \nOver and over, practice makes perfect \nBut not if you're a loafer. \n\nYou'll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin' \nVanilla Ice is sellin' and you people are buyin' \n'Cause why the freaks are jockin' like Crazy Glue \nMovin' and groovin' trying to sing along \nAll through the ghetto groovin' this here song \nNow you're amazed by the VIP posse. \n\nSteppin' so hard like a German Nazi \nStartled by the bases hittin' ground \nThere's no trippin' on mine, I'm just gettin' down \nSparkamatic, I'm hangin' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n'89 in my time! You, '90 is my year. \n\nYou're weakenin' fast, YO! and I can tell it \nYour body's gettin' hot, so, so I can smell it \nSo don't be mad and don't be sad \n'Cause the lyrics belong to ICE, You can call me Dad \nYou're pitchin' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don't be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you're dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n\x04\x04\x04\x04",
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			cipher := mustBase64DecodeFile(tc.file)
+			have := CBCDecrypt(cipher, []byte(tc.key), []byte(tc.iv))
+			equalString(t, string(have), tc.expect)
 		})
 	}
 }
